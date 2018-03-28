014495ca0b1880a94c208835985948263fd75c22
Don Chalanga Kuruppu <donchalangakuruppu@Dons-iMac.local>
2018-03-28 12:51:36 -0500

    Moved the child objects created up relative to the parent and scaled them down by 0.5 relative to the parent.

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -7,8 +7,10 @@ public class Fractal : MonoBehaviour {
     public Mesh mesh;
     public Material material;
 
-    public int maxDepth;            // Indicates the max number of levels to go down recursively
-    private int currentDepth;       // Indicates the depth which the current object is at             
+    public int maxDepth;                // Indicates the max number of levels to go down recursively
+    private int currentDepth = 0;       // Indicates the depth which the current object is at   
+
+    public float childScale;            // How much to scale the child by
 
 	// Use this for initialization
 	private void Start () {
@@ -22,13 +24,21 @@ public class Fractal : MonoBehaviour {
 	}
 
     // Used to create child objects that inherit parent properties
-    private void Initialize(Fractal parent)
-    {
+    private void Initialize(Fractal parent) {
         mesh = parent.mesh;
         material = parent.material;
         maxDepth = parent.maxDepth;
         currentDepth = parent.currentDepth + 1;
-        transform.parent = parent.transform;
+        childScale = parent.childScale;
+
+        // To make Fractal parent the parent of what is to be created
+        gameObject.transform.SetParent(parent.transform, false);
+
+        // Scale the child 
+        transform.localScale = Vector3.one * childScale;
+
+        // Move the child so that once moved that are in contact
+        transform.localPosition = Vector3.up * (0.5f + 0.5f * childScale);
     }
 	
 	// Update is called once per frame
