5fbb0fa9fe99331a9621de87141a093e07981601
Don Chalanga Kuruppu <donchalangakuruppu@Dons-iMac.local>
2018-03-28 11:37:58 -0500

    Implemented creating children that inherit parent properties recursively, successfully.

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -7,11 +7,29 @@ public class Fractal : MonoBehaviour {
     public Mesh mesh;
     public Material material;
 
+    public int maxDepth;            // Indicates the max number of levels to go down recursively
+    private int currentDepth;       // Indicates the depth which the current object is at             
+
 	// Use this for initialization
-	void Start () {
+	private void Start () {
         gameObject.AddComponent<MeshFilter>().mesh = mesh;
         gameObject.AddComponent<MeshRenderer>().material = material;
+
+        // If less than maximum  depth, create more children
+        if (currentDepth < maxDepth) {
+            new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this);
+        }
 	}
+
+    // Used to create child objects that inherit parent properties
+    private void Initialize(Fractal parent)
+    {
+        mesh = parent.mesh;
+        material = parent.material;
+        maxDepth = parent.maxDepth;
+        currentDepth = parent.currentDepth + 1;
+        transform.parent = parent.transform;
+    }
 	
 	// Update is called once per frame
 	void Update () {
